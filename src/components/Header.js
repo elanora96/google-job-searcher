8a385eaf66babf9278e1fe34b08e8ed2a4012b79
Burk Manson <burkmanson@gmail.com>
2022-08-31 15:55:13 -0700

    Added Github link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import { Navbar } from 'react-bootstrap';
-import { BsSearch } from 'react-icons/bs';
+import { Navbar, Container } from 'react-bootstrap';
+import { BsSearch, BsGithub } from 'react-icons/bs';
 
 const Header = (props) => {
   return (
@@ -7,6 +7,11 @@ const Header = (props) => {
       <Navbar.Brand>
         <BsSearch /> Google Job Searcher
       </Navbar.Brand>
+      <Container style={{ display: 'flex', justifyContent: 'right' }}>
+        <a style={{textDecoration: 'none'}} href="https://github.com/burk96/google-job-searcher">
+          <BsGithub />
+        </a>
+      </Container>
     </Navbar>
   );
 };
